0601a56ecd16818ef1aa041c44b09f97c47e6511
juno <juno@krasava.fi>
2014-08-18 04:59:19 +0300

    dev

--- common_redis_feeder/src/cron_parser.go
+++ common_redis_feeder/src/cron_parser.go
@@ -1,15 +1,15 @@
-package main 
+package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-	"log/syslog"
+	"domains"
+	"flag"
+	"fmt"
 	"github.com/garyburd/redigo/redis"
-    "domains"
-    "yqljsonparser"
-    "parsebypath"
-    "redisin"
+	"log"
+	"log/syslog"
+	"parsebypath"
+	"redisin"
+	"yqljsonparser"
 )
 
 const APP_VERSION = "0.1"
@@ -18,59 +18,54 @@ const APP_VERSION = "0.1"
 var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
 
 func main() {
-    flag.Parse() // Scan the arguments list 
+	flag.Parse() // Scan the arguments list
 
-    if *versionFlag {
-        fmt.Println("Version:", APP_VERSION)
-                
-    }
-    
-    golog, err := syslog.New(syslog.LOG_ERR, "golog")
+	if *versionFlag {
+		fmt.Println("Version:", APP_VERSION)
+
+	}
+
+	golog, err := syslog.New(syslog.LOG_ERR, "golog")
 
 	defer golog.Close()
 	if err != nil {
 		log.Fatal("error writing syslog!!")
 
 	}
-	
-	
-		c, err := redis.Dial("tcp", ":6379")
+
+	c, err := redis.Dial("tcp", ":6379")
 	if err != nil {
-//		log.Fatal(err)
+		//		log.Fatal(err)
 		golog.Err(err.Error())
 	}
-	
-    
-  var rssLinks = []domains.RssLink {
-  	
-  	{Redisid: "en_US:finance:news:business",OrgLink: "select * from rss where url = 'http://feeds.reuters.com/reuters/businessNews'",LinktoParse:"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20%22http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FbusinessNews%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"}, 
-  	{Redisid: "en_US:finance:news:mostread",OrgLink: "select * from rss where url = 'http://feeds.reuters.com/reuters/MostRead'",LinktoParse:"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20'http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FMostRead'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"}, 
-  	  	
-  } 
-  
-  
-  for _,rsslink := range rssLinks {
-  	
-  	
-  	arrlinks :=yqljsonparser.Parser(*golog,rsslink.LinktoParse)
-  	
-  	for _,link := range arrlinks {
-  		
-  		
-  		xpath := []string{"//div[@class='relatedPhoto landscape']", "//span[@id='articleText']/p"}
-  		 		
-  		item := parsebypath.Parse(*golog,link,xpath)
-  		
-  		fmt.Println(item.ImgLink)
-  		fmt.Println(item.Cont)
-  		
-  		redisin.InsertIn(*golog, c,rsslink.Redisid,item )
-  		 		
-  		
-  	} 	
-  	  	
-  }
-    
-    
-}
 
+	var rssLinks = []domains.RssLink{
+
+		{Redisid: "en_US:finance:news:business", OrgLink: "select * from rss where url = 'http://feeds.reuters.com/reuters/businessNews'", LinktoParse: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20%22http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FbusinessNews%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"},
+		{Redisid: "en_US:finance:news:mostread", OrgLink: "select * from rss where url = 'http://feeds.reuters.com/reuters/MostRead'", LinktoParse: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20'http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FMostRead'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"},
+	}
+
+	for _, rsslink := range rssLinks {
+
+		arrlinks := yqljsonparser.Parser(*golog, rsslink.LinktoParse)
+
+		for _, link := range arrlinks {
+
+			xpath := []string{"//div[@class='relatedPhoto landscape']", "//span[@id='articleText']/p"}
+
+			item := parsebypath.Parse(*golog, link, xpath)
+
+			fmt.Println(item.ImgLink)
+			fmt.Println(item.Cont)
+
+			if item.ImgLink != "" {
+
+				redisin.InsertIn(*golog, c, rsslink.Redisid, item)
+
+			}
+
+		}
+
+	}
+
+}
--- common_redis_feeder/src/parsebypath/parsebypath.go
+++ common_redis_feeder/src/parsebypath/parsebypath.go
@@ -44,8 +44,6 @@ func Parse(golog syslog.Writer, item domains.Item, xpath []string) domains.Item 
 			golog.Err(err.Error())
 		} else {
 			imglink := res2[0].Attr("src")
-
-//			fmt.Println(imglink)
 			item.ImgLink = imglink
 
 		}
@@ -57,8 +55,6 @@ func Parse(golog syslog.Writer, item domains.Item, xpath []string) domains.Item 
 	var content string
 
 	for i, itemdom := range res {
-
-//		fmt.Println(itemdom.Content())
 
 		if i == 0 {
 
@@ -76,89 +72,5 @@ func Parse(golog syslog.Writer, item domains.Item, xpath []string) domains.Item 
 	
 	return item
 
-	//	var redisidItemsarrret []domains.RedisidItems
-	//
-	//	for _, redisidItems := range redisidItemsarr {
-	//
-	//		var redisidItemsret domains.RedisidItems
-	//
-	//		redisidItemsret.RedisID = redisidItems.RedisID
-	//
-	//		var returnitemsarr []domains.Item
-	//
-	//		for _, item := range redisidItems.Items {
-	//
-	//			newitem := item
-	//
-	//			req, err := http.NewRequest("GET", item.Link, nil)
-	//			if err != nil {
-	//				golog.Err(err.Error())
-	//
-	//			}
-	//
-	//			resp, err := client.Do(req)
-	//			if err != nil {
-	//
-	//				golog.Err(err.Error())
-	//			}
-	//
-	//			defer resp.Body.Close()
-	//			bodybytes, err := ioutil.ReadAll(resp.Body)
-	//
-	//			if err != nil {
-	//				golog.Err(err.Error())
-	//			}
-	//			doc, _ := gokogiri.ParseHtml([]byte(bodybytes))
-	//			res, _ := doc.Search(xpath[0])
-	//
-	//			for _, itemdom := range res {
-	//
-	//				if res2, err := itemdom.Search("img"); err != nil {
-	//
-	//					golog.Err(err.Error())
-	//				} else {
-	//
-	//					imglink := res2[0].Attr("src")
-	//					newitem.ImgLink = imglink
-	//
-	//					if strings.HasSuffix(newitem.ImgLink, ".jpg") && strings.HasPrefix(item.Link, "http://www.quotidiano.net") {
-	//
-	//						//			if strings.HasSuffix(newitem.ImgLink, ".jpg") {
-	//						//				imgLink := newitem.Link[0:strings.Index(newitem.Link, "-")] + "/" + newitem.ImgLink[strings.Index(newitem.ImgLink, "images"):len(newitem.ImgLink)]
-	//						//				newitem.ImgLink = imgLink
-	//						newitem.ImgLink = "http://www.quotidiano.net" + newitem.ImgLink
-	//						//						returnitemsarr = append(returnitemsarr, newitem)
-	//
-	//						res, _ = doc.Search(xpath[1])
-	//
-	//						var cont string
-	//						for i, itemdom := range res {
-	//
-	//							if i == 0 {
-	//								cont = itemdom.Content()
-	//							} else {
-	//
-	//								cont = cont + " " + itemdom.Content()
-	//							}
-	//							newitem.Cont = cont
-	//
-	//						}
-	//
-	//						if newitem.ImgLink != "" && newitem.Cont != "" {
-	//							returnitemsarr = append(returnitemsarr, newitem)
-	//
-	//						}
-	//
-	//					}
-	//
-	//				}
-	//			}
-	//
-	//			redisidItemsret.Items = returnitemsarr
-	//		}
-	//
-	//		redisidItemsarrret = append(redisidItemsarrret, redisidItemsret)
-	//
-	//	}
-	//	return redisidItemsarrret
+
 }
