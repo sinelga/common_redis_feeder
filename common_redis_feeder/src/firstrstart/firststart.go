31986c99dd2b3a079ceedf5742d5685f08dbcb27
juno <juno@krasava.fi>
2014-08-22 11:27:49 +0300

    dev

--- common_redis_feeder/src/firstrstart/firststart.go
+++ common_redis_feeder/src/firstrstart/firststart.go
@@ -59,36 +59,7 @@ func FeedRedis(golog syslog.Writer, c redis.Conn, locale string, themes string) 
 		//		log.Fatal(err)
 		golog.Crit(err.Error())
 	}
-//	fmt.Println(string(buf))
 
-	//	var en_US_finance_menu_news = []domains.Menu{
-	//		{Menu: "Art", OrgLink: "http://feeds.reuters.com/news/artsculture", ParsLink: "en_US:finance:news:art"},
-	//		{Menu: "Business", OrgLink: "http://feeds.reuters.com/reuters/businessNews", ParsLink: "en_US:finance:news:business"},
-	//		{Menu: "Business Travel", OrgLink: "http://feeds.reuters.com/ReutersBusinessTravel", ParsLink: "en_US:finance:news:businesstravel"},
-	//		{Menu: "Company News", OrgLink: "http://feeds.reuters.com/reuters/companyNews", ParsLink: "en_US:finance:news:companynews"},
-	//		{Menu: "Entertainment", OrgLink: "http://feeds.reuters.com/reuters/entertainment", ParsLink: "en_US:finance:news:entertainment"},
-	//		{Menu: "Environment", OrgLink: "http://feeds.reuters.com/reuters/environment", ParsLink: "en_US:finance:news:environment"},
-	//		{Menu: "Most Read Articles", OrgLink: "http://feeds.reuters.com/reuters/MostRead", ParsLink: "en_US:finance:news:mostread"},
-	//	}
-	//
-	//	var en_US_finance_menu_markets = []domains.Menu{
-	//		{Menu: "Bankruptcy", OrgLink: "http://feeds.reuters.com/reuters/bankruptcyNews", ParsLink: "en_US:finance:markets:bankruptcy"},
-	//		{Menu: "Bonds", OrgLink: "http://feeds.reuters.com/reuters/bondsNews", ParsLink: "en_US:finance:markets:bonds"},
-	//	}
-	//
-	//
-	//	var en_US_finance_tab = []domains.Tab{
-	//		{Tab: "News", Menus: en_US_finance_menu_news},
-	//		{Tab: "Markets", Menus: en_US_finance_menu_markets},
-	//	}
-	//
-	//	var en_US_finance_localethemes = domains.LocaleThemes{Tabs: en_US_finance_tab}
-	//
-	//		b, err := json.Marshal(en_US_finance_localethemes)
-	//		if err != nil {
-	//
-	//			golog.Crit(err.Error())
-	//		}
 
 	if _, err := c.Do("SET", "en_US:finance", buf); err != nil {
 
--- common_redis_feeder/usefull.bash
+++ common_redis_feeder/usefull.bash
@@ -4,3 +4,6 @@ GOPATH=$GOPATH:/home/juno/git/common_redis_feeder/common_redis_feeder go test -v
 ZRANGEBYSCORE en_US:finance:news:business -inf +inf withscores  LIMIT 0 2000
 
 ZCOUNT en_US:finance:news:business -inf +inf
+
+ bin/tabmenufeedup -locale=en_US -themes=finance
+ bin/cron_parser -locale=en_US -themes=finance
